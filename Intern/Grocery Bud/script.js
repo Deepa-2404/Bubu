bfd8a2ec32682bcbac6c5906b430292283ba9150
Deepa-2404 <deepadharshini2404@gmail.com>
2025-05-21 20:41:35 +0530

    some files are modified

--- Intern/Grocery Bud/script.js
+++ Intern/Grocery Bud/script.js
@@ -4,21 +4,25 @@ function addItem()
   let input = document.getElementById("item-input");
   let list = document.getElementById("item-list");
 
-  if (input.value === "") return;
+  if (input.value.trim() === "") return;
 
   let li = document.createElement("li");
-  li.textContent = input.value;
+  
+  let span = document.createElement("span");
+  span.textContent = input.value;
 
   let removeBtn = document.createElement("button");
   removeBtn.textContent = "Remove";
   removeBtn.onclick = function () {
-    list.remove(li);
+    
+    list.removeChild(li);
   };
+li.appendChild(span);
+  li.appendChild(removeBtn);
 
-  li.textContent = ""; 
-  li.append(input.value + " ", removeBtn);
-  list.append(li);
+  list.appendChild(li);
   input.value = "";
+  
 }
 
 function clearList() {
